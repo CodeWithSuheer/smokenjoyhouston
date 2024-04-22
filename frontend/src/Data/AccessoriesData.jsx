293f1d728fec04d1d4bb2f14b9c29c8e4d06ef11
Suheer Zahid <suheer.work18@gmail.com>
2024-04-23 00:04:55 +0500

    Add Accessories Data

---
+++ frontend/src/Data/AccessoriesData.jsx
@@ -0,0 +1,70 @@
+const AccessoriesData = [
+  {
+    id: 51,
+    name: "Stinger Detok",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/stinger_detok.jpg?v=1713811424",
+  },
+  {
+    id: 52,
+    name: "Qcarbo",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/Qcarbo.jpg?v=1713811424",
+  },
+  {
+    id: 53,
+    name: "Puriclean Instant",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/puriclean_instant_complete_body_cleaner.jpg?v=1713811424",
+  },
+  {
+    id: 54,
+    name: "Colorado Crusher",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/colorado_crusher.webp?v=1713811423",
+  },
+  {
+    id: 55,
+    name: "Storz and Bickel Hybrid Volcano Vaporizer",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/storz-and-bickel-hybrid-volcano-vaporizer-5-576003.webp?v=1713811423",
+  },
+  {
+    id: 56,
+    name: "Truweigh MINI CLASSIC Digital Scale",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/Truweigh_MINI_CLASSIC_Digital_Scale.jpg?v=1713811423",
+  },
+  {
+    id: 57,
+    name: "Daimond Grind",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/images_1.jpg?v=1713811423",
+  },
+  {
+    id: 58,
+    name: "Ultra Purewhip",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/ultra_purewhip.webp?v=1713811423",
+  },
+  {
+    id: 59,
+    name: "Pulsar Grinder",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/images.jpg?v=1713811423",
+  },
+  {
+    id: 60,
+    name: "RYOT dugout",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/ryot-dugout.jpg?v=1713811422",
+  },
+  {
+    id: 61,
+    name: "Whip Cream Tank",
+    image:
+      "https://cdn.shopify.com/s/files/1/0649/1399/8024/files/whip_cream_tank.webp?v=1713811422",
+  },
+];
+
+export { AccessoriesData };
